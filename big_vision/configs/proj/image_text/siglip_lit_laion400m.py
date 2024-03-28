6cdca1ad508434ddeb2586d9007a1c16f8511dee
Ziteng <zw2526@nyu.edu>
2024-03-28 20:22:12 +0000

    some update over siglip and siglit training configuration

--- big_vision/configs/proj/image_text/siglip_lit_laion400m.py
+++ big_vision/configs/proj/image_text/siglip_lit_laion400m.py
@@ -1,29 +1,24 @@
-r"""SigLIP (https://arxiv.org/abs/2303.15343) Replication.
+# Copyright 2024 Big Vision Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-Training receipe:
-- train dataset: laion400m/images
-  - metadata info for the downloaded data in gcloud storage:
-    - total_num_bytes: 7,927,590,520,336 (7.9TB)
-    - len(shard_lengths): 62,917 (number of tfrecord files)
-    - total_samples: 327,702,052 (328M)
-  - train
-    - image resolution: 224*224
-    - tokenizer: 32K vocabulary sentencepiece; trained on C4 dataset; output has 16 maximum tokens 
-    - batch size: 32,768 (TPUv4-32), 4,096 (TPUv4-8 & SigLIP Base), 2,048 (TPUv4-8 & CLIP Base)
-    - optimizer: β2 = 0.95 (not 0.999, stablizing the training)
-    - strategy: from scratch < unlocked ft. on pre-trained ViT-AugReg-B/16 < ft. without weight decay (Figure 4)
-    - combo: TPUv4-32 & 16,384 & 2.4B (Figure 4); 
-  - model
-    - image
-      - ViT-B/16 # TO_LEARN: what is ViT-AugReg-B/16
-      - TO_DETERMINE: head or no head?
-      - randomly initialized
-      - from scratch
+# pylint: disable=line-too-long
+r"""Minimal SigLIP (https://arxiv.org/abs/2303.15343) example.
 
-Bash for training:
+Example training:
 
 big_vision.trainers.proj.image_text.siglip \
-    --config big_vision/configs/proj/image_text/siglip_replication.py:batch_size=512 \
+    --config big_vision/configs/proj/image_text/lit_coco.py:batch_size=512 \
     --workdir gs://$GS_BUCKET_NAME/big_vision/`date '+%Y-%m-%d_%H%M'`
 """
 
@@ -36,7 +31,7 @@ def get_config(arg=None):
   """The base configuration."""
   arg = bvcc.parse_arg(
       arg, res=224, runlocal=False, token_len=16, txt='bert_base', img='B/16',
-      init='', img_head=False, batch_size=4096)
+      init='', img_head=False, batch_size=512)# 32_768)
   img_name, img_init = common.inits[arg.img]
   txt_name, txt_init = common.inits[arg.txt]
   config = ConfigDict()
@@ -45,13 +40,11 @@ def get_config(arg=None):
   config.input.data = dict(name='laion400m/images', split='train')
   config.input.batch_size = arg.batch_size if not arg.runlocal else 32
   config.input.shuffle_buffer_size = 250_000  if not arg.runlocal else 50
-  config.input.pack = True # TO_DETERMINE: pack or not pack? TODO: examine the variance of sequence length
 
-  # config.total_steps = 80006 if not arg.runlocal else 1 # 1 epoch
-  config.total_steps = 45 if not arg.runlocal else 1 # DEBUG 183864/4096=44.888671875
+  config.total_steps = 65_000 if not arg.runlocal else 1
 
-  config.init_shapes = [(1, arg.res, arg.res, 3), (1, arg.token_len,)] # TO_LEARN: where is it used?
-  config.init_types = ['float32', 'int32'] # TO_LEARN: where is it used?
+  config.init_shapes = [(1, arg.res, arg.res, 3), (1, arg.token_len,)]
+  config.init_types = ['float32', 'int32']
 
   if arg.init:
     vocab_path = arg.init.rsplit('.', 1)[0] + '.txt'
@@ -82,7 +75,7 @@ def get_config(arg=None):
       config.model_load['img_load_kw'] = {'dont_load': ['head/kernel', 'head/bias']}
   config.model = ConfigDict()
   config.model.image_model = 'vit'
-  config.model.text_model = 'proj.image_text.text_transformer'
+  config.model.text_model = 'proj.flaxformer.bert'
   config.model.image = ConfigDict({
       'variant': img_name,
       'pool_type': 'tok',
@@ -99,15 +92,18 @@ def get_config(arg=None):
 
   if txt_name == 'base':
     config.optax_name = 'scale_by_adam'
+    config.optax = dict(b2=0.95) # TO_ADD
   else:
     config.optax_name = 'big_vision.scale_by_adafactor'
+    config.optax = dict(beta2_cap=0.95) # TO_ADD
 
-  config.lr = 0.001
-  config.wd = 0.01
-  warmup_steps = max(int(0.03 * config.total_steps), 100)
+
+  config.lr = 1e-4
+  config.wd = 1e-7
+  warmup_steps = max(int(0.1 * config.total_steps), 100) # 6_500 linearly warmup steps
   config.schedule = [
       ('img/.*', None),  # Freezes image tower.
-      ('.*', dict(decay_type='cosine', warmup_steps=warmup_steps)),
+      ('.*', dict(decay_type='cosine', warmup_steps=warmup_steps,mult=1e-6)), # TO_DETERMINE: 1.0 or a very small value?
   ]
 
   config.grad_clip_norm = 1.0
--- scripts/siglip.sh
+++ scripts/siglip.sh
@@ -4,4 +4,5 @@ export TFDS_DATA_DIR=gs://us-central2-storage/tensorflow_datasets
 
 cd ~/big_vision
 python -m big_vision.trainers.proj.image_text.siglip \
-    --config big_vision/configs/proj/image_text/siglip_replication.py
+    --config big_vision/configs/proj/image_text/siglip_lit_laion400m.py 
+    # siglip_lit_laion400m, siglip_lit_coco, siglip_replication
